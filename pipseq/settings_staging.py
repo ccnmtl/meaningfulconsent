73b190b4956dd4e4c58ee301832221de6e8d3802
Susan Dreher <sdreher@columbia.edu>
2014-01-16 15:00:03 -0500

    pipseq initial

---
+++ pipseq/settings_staging.py
@@ -0,0 +1,50 @@
+# flake8: noqa
+from settings_shared import *
+
+TEMPLATE_DIRS = (
+    "/var/www/pipseq/pipseq/pipseq/templates",
+)
+
+MEDIA_ROOT = '/var/www/pipseq/uploads/'
+# put any static media here to override app served static media
+STATICMEDIA_MOUNTS = (
+    ('/sitemedia', '/var/www/pipseq/pipseq/sitemedia'),
+)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pipseq',
+        'HOST': '',
+        'PORT': 6432,
+        'USER': '',
+        'PASSWORD': '',
+    }
+}
+
+COMPRESS_ROOT = "/var/www/pipseq/pipseq/media/"
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+STAGING_ENV = True
+
+STATSD_PREFIX = 'pipseq-staging'
+SENTRY_SITE = 'pipseq-staging'
+SENTRY_SERVERS = ['http://sentry.ccnmtl.columbia.edu/sentry/store/']
+
+if 'migrate' not in sys.argv:
+    INSTALLED_APPS.append('raven.contrib.django')
+
+    import logging
+    from raven.contrib.django.handlers import SentryHandler
+    logger = logging.getLogger()
+    # ensure we havent already registered the handler
+    if SentryHandler not in map(type, logger.handlers):
+        logger.addHandler(SentryHandler())
+        logger = logging.getLogger('sentry.errors')
+        logger.propagate = False
+        logger.addHandler(logging.StreamHandler())
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
