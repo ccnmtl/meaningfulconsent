2b5a64715f89c458c4ab5f39c7fb3ef6905251b9
Anders Pearson <anders@columbia.edu>
2015-03-13 12:45:02 +0100

    1.7 migrations

--- meaningfulconsent/main/migrations/0001_initial.py
+++ meaningfulconsent/main/migrations/0001_initial.py
@@ -1,17 +1,82 @@
+# flake8: noqa
 # -*- coding: utf-8 -*-
-from south.v2 import SchemaMigration
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        pass
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
 
-    def backwards(self, orm):
-        pass
-
-    models = {
-
-    }
-
-    complete_apps = ['main']
+    operations = [
+        migrations.CreateModel(
+            name='Clinic',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=25)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='QuizSummaryBlock',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('quiz_class', models.CharField(max_length=50)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='UserProfile',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('language', models.CharField(default=b'en', max_length=2, choices=[(b'en', b'English'), (b'es', b'Spanish')])),
+                ('archived', models.BooleanField(default=False)),
+                ('notes', models.TextField(null=True, blank=True)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('modified', models.DateTimeField(auto_now=True)),
+                ('clinic', models.ForeignKey(to='main.Clinic')),
+                ('creator', models.ForeignKey(related_name='creator', blank=True, to=settings.AUTH_USER_MODEL, null=True)),
+                ('user', models.OneToOneField(related_name='profile', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='UserVideoView',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('video_id', models.CharField(max_length=256)),
+                ('video_duration', models.IntegerField(default=0)),
+                ('seconds_viewed', models.IntegerField(default=0)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='YouTubeBlock',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('video_id', models.CharField(max_length=256)),
+                ('language', models.CharField(max_length=2, choices=[(b'en', b'English'), (b'es', b'Spanish')])),
+                ('title', models.TextField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AlterUniqueTogether(
+            name='uservideoview',
+            unique_together=set([('user', 'video_id')]),
+        ),
+    ]
