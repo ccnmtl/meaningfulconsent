0dd01de97dd381c74358da8df4419f7569c93d1a
Nikolas Nyby <nikolas@gnu.org>
2019-11-08 13:00:21 -0500

    migrate to sentry-sdk

--- meaningfulconsent/settings_production.py
+++ meaningfulconsent/settings_production.py
@@ -1,18 +1,26 @@
-# flake8: noqa
-from meaningfulconsent.settings_shared import *
+from django.conf import settings
+from meaningfulconsent.settings_shared import *  # noqa: F403
 from ccnmtlsettings.production import common
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 locals().update(
     common(
-        project=project,
-        base=base,
-        INSTALLED_APPS=INSTALLED_APPS,
-        STATIC_ROOT=STATIC_ROOT,
+        project=project,  # noqa: F405
+        base=base,  # noqa: F405
+        INSTALLED_APPS=INSTALLED_APPS,  # noqa: F405
+        STATIC_ROOT=STATIC_ROOT,  # noqa: F405
     ))
 
 LOCALE_PATHS = ('/var/www/meaningfulconsent/meaningfulconsent/locale',)
 
 try:
-    from meaningfulconsent.local_settings import *
+    from meaningfulconsent.local_settings import *  # noqa: F403
 except ImportError:
     pass
+
+if hasattr(settings, 'SENTRY_DSN'):
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,  # noqa: F405
+        integrations=[DjangoIntegration()],
+    )
